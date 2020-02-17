f6b3eae0bb5e020a49369de9eab4468c2f21ff90
harshad <arade.harshad@gmail.com>
2020-02-17 14:54:41 -0500

    html-loader and file-loader for img files to be in same directory of dist. clean-webpack-plugin to clean dist each prod build

--- webpack.common.js
+++ webpack.common.js
@@ -17,6 +17,21 @@ module.exports = {
                     "sass-loader" //1. loads sass through node-sass bootstrap converts to css
                 ]
 
+            },
+            {
+                test: /\.html$/,
+                use: ["html-loader"]
+            },
+            {
+                test: /\.(svg|png|jpg|gif)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        esModule: false,
+                        name: "[name].[hash].[ext]",
+                        outputPath: "imgs"
+                    }
+                }
             }
         ]
     }
