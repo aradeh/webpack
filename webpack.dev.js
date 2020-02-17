a51d7e0282b32c5237c99dbffe5ad4a0989bad7a
harshad <arade.harshad@gmail.com>
2020-02-17 15:39:07 -0500

    vendor.js for entry point and jquery popper dependency

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,13 +2,11 @@ const path = require("path");
 const common = require("./webpack.common");
 const merge = require("webpack-merge");
 
-var HtmlWebpackPlugin = require("html-webpack-plugin");
-
 module.exports = merge(common, {
     mode: "development",
     
     output: {
-        filename: "main.js",
+        filename: "[name].bundle.js",
         path: path.resolve(__dirname,"dist")
     }
 });
